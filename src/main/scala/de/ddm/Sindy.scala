99e45177862823a3e6b4212a8741d41994fbe3a0
indri <berbiuindrit5@gmail.com>
2024-02-01 22:57:23 +0100

    Competed discoverINDs

--- src/main/scala/de/ddm/Sindy.scala
+++ src/main/scala/de/ddm/Sindy.scala
@@ -15,6 +15,23 @@ object Sindy {
   }
 
   def discoverINDs(inputs: List[String], spark: SparkSession): Unit = {
+    import spark.implicits._
+
+    inputs.map(input => readData(input, spark))
+      .map(table => {
+        val attributes = table.columns
+        table.flatMap(row => attributes.indices.map(x => (attributes(x), row.getString(x))))
+      }).reduce((pair1, pair2) => pair1 union pair2)
+      .groupByKey(t => t._2)
+      .mapGroups((_, iterator) => iterator.map(x => x._1).toSet)
+      .flatMap(s => s.map(elem => (elem, s - elem)))
+      .groupByKey(row => row._1)
+      .mapGroups((key, iter) => (key, iter.map(x => x._2).reduce(_ intersect _)))
+      .collect()
+      .filter(_._2.nonEmpty)
+      .map(row => (row._1, row._2.toList.sorted))
+      .sortBy(_._1)
+      .foreach(row => println(row._1 + " < " + row._2.mkString(", ")))
 
     // TODO
   }
